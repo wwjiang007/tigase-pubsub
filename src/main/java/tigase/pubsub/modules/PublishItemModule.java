243d4f08b829a57b053bbf3aab156984026c9048
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2014-05-21 17:54:58 +0200

    fix missing import after merge

--- src/main/java/tigase/pubsub/modules/PublishItemModule.java
+++ src/main/java/tigase/pubsub/modules/PublishItemModule.java
@@ -24,6 +24,7 @@ package tigase.pubsub.modules;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
