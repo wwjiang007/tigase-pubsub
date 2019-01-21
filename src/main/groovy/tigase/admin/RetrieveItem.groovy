842bfc29e23fff379acfe012d3644d6620ee99e8
Wojciech Kapcia <wojtek@tigase.org>
2019-01-21 15:39:38 -0300

    #8483 update licence headers

--- src/main/groovy/tigase/admin/RetrieveItem.groovy
+++ src/main/groovy/tigase/admin/RetrieveItem.groovy
@@ -1,6 +1,6 @@
-/*
- * Tigase Jabber/XMPP Server
- * Copyright (C) 2004-2016 "Tigase, Inc." <office@tigase.com>
+/**
+ * Tigase PubSub - Publish Subscribe component for Tigase
+ * Copyright (C) 2008 Tigase, Inc. (office@tigase.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -14,12 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
- *
- * $Rev: $
- * Last modified by $Author: $
- * $Date: $
  */
-
 /*
  Retrieve PubSub node item
 
--- src/main/groovy/tigase/pubsub/repository_migrator.sh
+++ src/main/groovy/tigase/pubsub/repository_migrator.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# Tigase PubSub - Publish Subscribe component for Tigase
+# Copyright (C) 2008 Tigase, Inc. (office@tigase.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. Look for COPYING file in the top folder.
+# If not, see http://www.gnu.org/licenses/.
+#
+
 LIBS="/home/smoku/workspace/tigase-server/target /home/smoku/workspace/tigase-server/libs /home/smoku/workspace/tigase-pubsub/target /usr/share/tigase-server/lib"
 for DIR in $LIBS; do CLASSPATH="`ls -d $DIR/*.jar 2>/dev/null | tr '\n' :`$CLASSPATH"; done
 export CLASSPATH
