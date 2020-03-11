c08f5dad5a816e25cd2e2adb460b6bb179012182
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2020-03-11 10:43:32 +0100

    Improvements to stored procedures for MySQL #mix-3

--- src/main/database/mysql-pubsub-5.0.0.sql
+++ src/main/database/mysql-pubsub-5.0.0.sql
@@ -74,10 +74,6 @@ drop function if exists TigPubSubEnsureServiceJid;
 drop procedure if exists TigPubSubCreateNode;
 -- QUERY END:
 
--- QUERY START:
-drop procedure if exists TigPubSubRemoveService;
--- QUERY END:
-
 delimiter //
 
 -- QUERY START:
@@ -105,40 +101,24 @@ begin
 	declare _node_id bigint;
 	declare _exists int;
 
--- Exception handler was intentionally removed as its behavior was wrong - we need exception on collision!
+	DECLARE exit handler for sqlexception
+		BEGIN
+			-- ERROR
+		ROLLBACK;
+		RESIGNAL;
+	END;
+
+	START TRANSACTION;
+	select TigPubSubEnsureJid(_node_creator) into _node_creator_id;
+	COMMIT;
 
 	START TRANSACTION;
 	select TigPubSubEnsureServiceJid(_service_jid, _domain, _createService) into _service_id;
-	select TigPubSubEnsureJid(_node_creator) into _node_creator_id;
 
 	insert into tig_pubsub_nodes (service_id,name,name_sha1,`type`,creator_id, creation_date, configuration,collection_id)
 			values (_service_id, _node_name, sha1(_node_name), _node_type, _node_creator_id, _ts, _node_conf, _collection_id);
 	select LAST_INSERT_ID() into _node_id;
 	select _node_id as node_id;
-	COMMIT;
-end //
--- QUERY END:
-
--- QUERY START:
-create procedure TigPubSubRemoveService(_service_jid varchar(2049))
-begin
-	declare _service_id bigint;
-
--- Exception handler was intentionally removed as its behavior was wrong - we need exception on collision!
-
-	START TRANSACTION;
-	select service_id into _service_id from tig_pubsub_service_jids
-		where service_jid_sha1 = SHA1(LOWER(_service_jid));
-	delete from tig_pubsub_items where node_id in (
-		select n.node_id from tig_pubsub_nodes n where n.service_id = _service_id);
-	delete from tig_pubsub_affiliations where node_id in (
-		select n.node_id from tig_pubsub_nodes n where n.service_id = _service_id);
-	delete from tig_pubsub_subscriptions where node_id in (
-		select n.node_id from tig_pubsub_nodes n where n.service_id = _service_id);
-	delete from tig_pubsub_nodes where service_id = _service_id;
-	delete from tig_pubsub_service_jids where service_id = _service_id;
-	delete from tig_pubsub_affiliations where jid_id in (select j.jid_id from tig_pubsub_jids j where j.jid_sha1 = SHA1(LOWER(_service_jid)));
-	delete from tig_pubsub_subscriptions where jid_id in (select j.jid_id from tig_pubsub_jids j where j.jid_sha1 = SHA1(LOWER(_service_jid)));
 	COMMIT;
 end //
 -- QUERY END:
@@ -227,6 +207,10 @@ drop procedure if exists TigPubSubGetServices;
 drop procedure if exists TigPubSubCreateService;
 -- QUERY END:
 
+-- QUERY START:
+drop procedure if exists TigPubSubRemoveService;
+-- QUERY END:
+
 delimiter //
 
 -- QUERY START:
@@ -257,15 +241,12 @@ create procedure TigPubSubWriteItem(_node_id bigint, _item_id varchar(1024) char
 	 _item_data mediumtext charset utf8mb4, _ts timestamp(6), _uuid varchar(36))
 begin
 	declare _publisher_id bigint;
-	DECLARE exit handler for sqlexception
-		BEGIN
-			-- ERROR
-		ROLLBACK;
-	END;
 
 	START TRANSACTION;
-
 	select TigPubSubEnsureJid(_publisher) into _publisher_id;
+	COMMIT;
+
+	START TRANSACTION;
 	insert into tig_pubsub_items (node_id, id_sha1, id, creation_date, update_date, publisher_id, data, uuid)
 		values (_node_id, SHA1(_item_id), _item_id, _ts, _ts, _publisher_id, _item_data, TigPubSubUuidToOrdered(_uuid))
 		on duplicate key update publisher_id = _publisher_id, data = _item_data, update_date = _ts, uuid = TigPubSubUuidToOrdered(_uuid);
@@ -436,6 +417,62 @@ create procedure TigPubSubCreateService(_service_jid varchar(2049), _domain varc
 begin
     insert into tig_pubsub_service_jids (service_jid, service_jid_sha1, domain, domain_sha1, is_public)
 			values (_service_jid, SHA1(LOWER(_service_jid)), _domain, SHA1(LOWER(_domain)), _is_public);
+end //
+-- QUERY END:
+
+-- QUERY START:
+create procedure TigPubSubRemoveService(_service_jid varchar(2049))
+begin
+	declare _service_id bigint;
+
+	DECLARE exit handler for sqlexception
+		BEGIN
+			-- ERROR
+		ROLLBACK;
+		RESIGNAL;
+	END;
+
+	START TRANSACTION;
+	select service_id into _service_id from tig_pubsub_service_jids
+		where service_jid_sha1 = SHA1(LOWER(_service_jid));
+	delete i
+	    from tig_pubsub_items i
+	    join tig_pubsub_nodes n on n.node_id = i.node_id
+	    join tig_pubsub_service_jids s on n.service_id = s.service_id
+	    where s.service_jid_sha1 = SHA1(LOWER(_service_jid));
+	delete a
+	    from tig_pubsub_affiliations a
+	    join tig_pubsub_nodes n on n.node_id = a.node_id
+	    join tig_pubsub_service_jids s on n.service_id = s.service_id
+	    where s.service_jid_sha1 = SHA1(LOWER(_service_jid));
+	delete sub
+	    from tig_pubsub_subscriptions sub
+	    join tig_pubsub_nodes n on n.node_id = sub.node_id
+	    join tig_pubsub_service_jids s on n.service_id = s.service_id
+	    where s.service_jid_sha1 = SHA1(LOWER(_service_jid));
+	delete m
+	    from tig_pubsub_mam m
+	    join tig_pubsub_nodes n on n.node_id = m.node_id
+	    join tig_pubsub_service_jids s on n.service_id = s.service_id
+	    where s.service_jid_sha1 = SHA1(LOWER(_service_jid));
+	delete n
+	    from tig_pubsub_nodes n
+	    join tig_pubsub_service_jids s on n.service_id = s.service_id
+	    where s.service_jid_sha1 = SHA1(LOWER(_service_jid));
+	delete from tig_pubsub_service_jids where service_jid_sha1 = SHA1(LOWER(_service_jid));
+	COMMIT;
+	START TRANSACTION;
+	delete a
+	    from tig_pubsub_affiliations a
+	    join tig_pubsub_jids j on j.jid_id = a.jid_id
+	    where j.jid_sha1 = SHA1(LOWER(_service_jid));
+	COMMIT;
+	START TRANSACTION;
+	delete s
+	    from tig_pubsub_subscriptions s
+	    join tig_pubsub_jids j on j.jid_id = s.jid_id
+	    where j.jid_sha1 = SHA1(LOWER(_service_jid));
+	COMMIT;
 end //
 -- QUERY END:
 
